fc9805f4ed29f96276fca018283c4431afd11c8f
puuval <puuval@puuval.me>
2023-05-31 06:59:30 -0400

    tdd done

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -2,9 +2,41 @@ package com.redhat.shipping;
 
 import org.junit.jupiter.api.Test;
 
+import com.redhat.shipping.ShippingCalculator.RegionNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
+/* 
+    @Test
+    public void canCalculateTheCostForARegion() throws RegionNotFoundException {
+        ShippingCalculator calculator = new ShippingCalculator();
+            assertEquals(0, calculator.costForRegion("A Region"));
+    }
+*/
+    @Test
+    public void onNARegionTheCostIs100() throws RegionNotFoundException {
+        assertEquals(100, (new ShippingCalculator()).costForRegion("NA"));
+    }
 
-    // @todo: add tests
+    @Test
+    public void onLATAMRegionTheCostIs200() throws RegionNotFoundException {
+        assertEquals(200, (new ShippingCalculator()).costForRegion("LATAM"));
+    }
+
+    @Test
+    public void onEMEARegionTheCostIs300() throws RegionNotFoundException {
+        assertEquals(300, (new ShippingCalculator()).costForRegion("EMEA"));
+    }
+
+    @Test
+    public void onAPACRegionTheCostIs400() throws RegionNotFoundException {
+        assertEquals(400, (new ShippingCalculator()).costForRegion("APAC"));
+    }
+
+    @Test
+    public void onNonSupportedRegionARegionNotFoundExceptionIsRaised() {
+        ShippingCalculator calculator = new ShippingCalculator();
+            assertThrows( RegionNotFoundException.class, () -> calculator.costForRegion("Unknown Region"));
+    }
 }
